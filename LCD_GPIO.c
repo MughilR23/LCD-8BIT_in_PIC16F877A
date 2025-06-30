aa56478b67a00c66f2c70334e537fe91c26c1dd5
Mughil R <137698038+MughilR23@users.noreply.github.com>
2025-06-30 15:09:53 +0530

    LCD_GPIO.c

--- LCD_GPIO.c
+++ LCD_GPIO.c
@@ -7,9 +7,9 @@ void main()
 LCD_init();
 LCD_clear();
 while(1){
-LCD_setcursor(0,5);
+LCD_setcursor(0,5);  //Oth ROW 5th COLOUMN
 LCD_print("Hello");
-LCD_setcursor(1,5);
+LCD_setcursor(1,5);  //1st ROW 5th COLOUMN 
 LCD_print("There!");
 __delay_ms(20000);
 LCD_clear();
